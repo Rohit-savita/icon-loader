3b65dc6425817be648db1576049dca3b2ea6681e
rohit <rohitkumar12345@hmhgf.com>
2022-04-28 20:24:32 +0530

    icon loader

---
+++ js/animation-loader.js
@@ -0,0 +1,17 @@
+  
+      //  $(document).ready(function(){
+        //  $("button").click(function(){
+          //  $("div").toggleClass("hoverAnimationloader");
+         // });
+       // });
+         $(document).ready(function(){
+          $("button").click(function(){
+            $(".showhide").hide();
+          });
+        });
+         $(document).ready(function(){
+          $(".button1").click(function(){
+            $(".showhide").show();
+          });
+        });
+  
